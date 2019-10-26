2364c6d7fd6fac96b76128ca7ef3a1a13be76906
famesjranko <andrewmcdonald42@gmail.com>
2019-10-26 15:16:21 +1100

    Update log_reset.sh

--- log_reset.sh
+++ log_reset.sh
@@ -9,11 +9,13 @@ wifi_log_reset="/home/pi/wifi.log2"
 network_log_reset="/home/pi/connection.log2"
 
 # reset both logs
-echo "resetting wifi.log"
+echo "resetting wifi.log. . ."
 cp $wifi_log_reset $wifi_log
-echo "resetting connection.log"
+echo "resetting connection.log. . ."
 cp $network_log_reset $network_log
 
+sleep 1
+echo ""
 ## count lines in logs to confirm reset
 lines_wifi=$(< $wifi_log wc -l)
 lines_connection=$(< $network_log wc -l)
@@ -29,14 +31,16 @@ lines_connection=$(< $network_log wc -l)
 ## print status
 if [ $lines_wifi == 1 ]
   then
-    echo "wifi log reset            [SUCCESS]"
+    echo "      wifi log reset      [ SUCCESS ]"
   else
-    echo "wifi log reset            [FAIL]"
+    echo "      wifi log reset      [ FAIL    ]"
 fi
 
 if [ $lines_connection == 1 ]
   then
-    echo "connection log reset      [SUCCESS]"
+    echo "connection log reset      [ SUCCESS ]"
   else
-    echo "connection log reset      [FAIL]"
+    echo "connection log reset      [ FAIL    ]"
 fi
+
+echo ""
