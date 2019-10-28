dc67db847bee1f40407bf54bb58b0d3a85a86d7c
famesjranko <andrewmcdonald42@gmail.com>
2019-10-28 21:14:17 +1100

    Update log_reset.sh

--- log_reset.sh
+++ log_reset.sh
@@ -1,33 +1,92 @@
 #!/bin/bash
 
-# full path to log file location
-wifi_log="/home/pi/wifi.log"
+Date=$(date '+%m-%d-%Y')
 
-# full path to log reset file location - backup logfile with 1 line header only
-wifi_log_reset="/home/pi/wifi_reset.log"
+## log file location
+log="/home/pi/wifi.log"
+
+## log backup destinateion
+backup="/home/pi/wifi.log."$Date
+
+## make sure backup log hasn't been made today
+find /home/pi/ -wholename $backup
+logexists=$?
+
+## ask user if today's log already exists
+if [ $logexists == 0 ]
+  then
+    read -n1 -p "do you want to overwrite? y/n" ans
+    case $ans in
+      y|Y) echo && echo "continuing with log overwrite . . .";;
+      n|N) echo && echo "exiting to prevent unwanted overwrite . . ."; exit ;;
+      *) echo && echo "unknown answer: exiting to be safe"; exit ;;
+    esac
+  else
+    ## create log backup file
+    touch $backup
+fi
+
+## backup log
+echo "backing up latest log to: " $backup
+cp $log $backup
+
+## confirm backup exists
+find /home/pi/ -wholename $backup
+filefound=$?
+
+## exit if backup not found
+if [ $filefound != 0 ]
+  then
+    echo "backup file not found, something went wrong, exiting . . ."
+	exit
+fi
+
+## confirm backup copy successful
+cmp --silent $log $backup
+match=$?
+
+if [ $match != 0 ]
+  then
+    echo "backup does not match original, exiting to prevent unwanted log deletion"
+    exit
+fi
 
 # reset log
 echo "resetting wifi.log . . ."
-cp $wifi_log_reset $wifi_log
+echo "### wifi log" > $log
 
 sleep 1
 echo ""
 
-## count lines in log to confirm reset - return 1 expected
-lines_wifi_log=$(< $wifi_log wc -l)
+## count lines in logs to confirm reset - return 1 expected
+lines_reset=$(< $log wc -l)
 
-## alternate count line methods
+## alternate count methods
   ## method 2
-    #lines_wifi=$(grep "" -c $wifi_log)
+     #lines_wifi=$(grep "" -c $wifi_log)
   ## method 3
-    #lines_wifi=$(wc -l $wifi_log | awk '{ print $1 }')
+     #lines_wifi=$(wc -l $wifi_log | awk '{ print $1 }')
 
 ## print status
-if [ $lines_wifi_log == 1 ]
+if [ $filefound -eq 0 ]
   then
-    echo "      wifi log reset      [ SUCCESS ]"
+    echo "      create backup         [ SUCCESS ]"
   else
-    echo "      wifi log reset      [ FAIL    ]"
+    echo "      create backup         [ FAIL    ]"
+fi
+
+if [ $match -eq 0 ]
+  then
+    echo "      backup matches log    [ SUCCESS ]"
+  else
+    echo "      backup matches log    [ FAIL    ]"
+fi
+
+if [ $lines_reset == 1 ]
+  then
+    echo "      reset log             [ SUCCESS ]"
+  else
+    echo "      reset log             [ FAIL    ]"
 fi
 
 echo ""
