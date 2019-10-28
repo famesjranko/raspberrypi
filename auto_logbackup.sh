8fa437df7868a52d9819af1ba6fb47b9586a47c7
famesjranko <andrewmcdonald42@gmail.com>
2019-10-29 01:17:16 +1100

    Update auto_logbackup.sh

--- auto_logbackup.sh
+++ auto_logbackup.sh
@@ -2,52 +2,44 @@
 
 ## current date/time
 date=$(date '+%m-%d-%Y')
-time=$(date '%T')
-
-## log file location
-log="/home/pi/wifi.log"
+time=$(date '+%T')
 
 ## log header
-header="### wifi log"
+header="### wifi log ###"
 
-## log backup destinateion
-backup="/home/pi/logs/wifi.log."$date
+## full path to log file
+log="/home/pi/wifi.log"
 
-## log directory
-dir="/home/pi/logs"
+## full path to backup directory
+dir="/home/pi/logs/"
 
-if [ ! -d "$dir" ] 
+## if log directory doesn't exist, make it
+if [ ! -d "$dir" ]
   then
     mkdir -p $dir
+   echo "1"
 fi
+
+## backup log name
+backup="wifi.log."$date
 
 ## make sure backup log hasn't been made today
-find /home/pi/logs -wholename $backup
-logexists=$?
+exists=$(find "$dir" -name "$backup")
 
 ## if today's log already exists, add timestamp, otherwise use date only
-if [ $logexists == 0 ]
+if [ "$exists" == "$dir$backup" ]
   then
-    backup=$backup.$time
-	touch $backup
+    ## create log backup file
+    touch $dir$backup.$time
+
+    ## backup log
+    cp $log $backup
   else
     ## create log backup file
-    touch $backup
-fi
+    touch $dir$backup
 
-## backup log
-cp $log $backup
-
-## confirm backup exists
-find /home/pi/ -wholename $backup
-filefound=$?
-
-## exit if backup not found
-if [ $filefound != 0 ]
-  then
-    # if backup file not found, exit to prevent unwanted log deletion
-	# could output failure to an error.log file if wanted
-	exit 1
+    ## backup log
+    cp $log $backup
 fi
 
 ## confirm backup copy successful
@@ -56,11 +48,13 @@ match=$?
 
 if [ $match != 0 ]
   then
+    echo "5"
     # if backup doesn't match original, exit to prevent unwanted log deletion
-	# could output failure to an error.log file if wanted
+    # could output failure to an error.log file if wanted
     exit 1
 fi
 
-## reset log
+## safe to reset log
 echo $header > $log
+
 exit 0
