14f3f9ab5f59f0392d01ff3a7842e84f6495ee49
famesjranko <andrewmcdonald42@gmail.com>
2019-10-28 19:22:44 +1100

    Update log_reset.sh

--- log_reset.sh
+++ log_reset.sh
@@ -2,45 +2,32 @@
 
 # log file location
 wifi_log="/home/pi/wifi.log"
-network_log="/home/pi/connection.log"
 
-# log reset file location
-wifi_log_reset="/home/pi/wifi.log2"
-network_log_reset="/home/pi/connection.log2"
+# log reset file location - backup logfile with 1 line header only
+wifi_log_reset="/home/pi/wifi_reset.log"
 
-# reset both logs
+# reset log
 echo "resetting wifi.log. . ."
 cp $wifi_log_reset $wifi_log
-echo "resetting connection.log. . ."
-cp $network_log_reset $network_log
 
 sleep 1
 echo ""
-## count lines in logs to confirm reset
-lines_wifi=$(< $wifi_log wc -l)
-lines_connection=$(< $network_log wc -l)
 
-## alternate count methods
-## method 2
-#lines_wifi=$(grep "" -c $wifi_log)
-#lines_connection=$(grep "" -c $network_log)
-## method 3
-#lines_wifi=$(wc -l $wifi_log | awk '{ print $1 }')
-#lines_connection=$(wc -l $network_log | awk '{ print $1 }')
+## count lines in logs to confirm reset - return 1 expected
+lines_wifi_log=$(< $wifi_log wc -l)
+
+## alternate count line methods
+  ## method 2
+    #lines_wifi=$(grep "" -c $wifi_log)
+  ## method 3
+    #lines_wifi=$(wc -l $wifi_log | awk '{ print $1 }')
 
 ## print status
-if [ $lines_wifi == 1 ]
+if [ $lines_wifi_log == 1 ]
   then
     echo "      wifi log reset      [ SUCCESS ]"
   else
     echo "      wifi log reset      [ FAIL    ]"
-fi
-
-if [ $lines_connection == 1 ]
-  then
-    echo "connection log reset      [ SUCCESS ]"
-  else
-    echo "connection log reset      [ FAIL    ]"
 fi
 
 echo ""
