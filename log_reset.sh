b631fa3065f98b0720bfc3d3886c6387bef4c86b
famesjranko <andrewmcdonald42@gmail.com>
2019-10-28 19:25:42 +1100

    Update log_reset.sh

--- log_reset.sh
+++ log_reset.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# log file location
+# full path to log file location
 wifi_log="/home/pi/wifi.log"
 
-# log reset file location - backup logfile with 1 line header only
+# full path to log reset file location - backup logfile with 1 line header only
 wifi_log_reset="/home/pi/wifi_reset.log"
 
 # reset log
@@ -13,7 +13,7 @@ cp $wifi_log_reset $wifi_log
 sleep 1
 echo ""
 
-## count lines in logs to confirm reset - return 1 expected
+## count lines in log to confirm reset - return 1 expected
 lines_wifi_log=$(< $wifi_log wc -l)
 
 ## alternate count line methods
