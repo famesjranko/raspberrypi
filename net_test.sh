1e82e87232ed59d654734cf96ea69bee3b320d44
famesjranko <andrewmcdonald42@gmail.com>
2019-11-02 21:40:14 +1100

    Update net_test.sh

--- net_test.sh
+++ net_test.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+## possible text colours
+BLACK=$(tput setaf 0)
+RED=$(tput setaf 1)
+GREEN=$(tput setaf 2)
+YELLOW=$(tput setaf 3)
+LIME_YELLOW=$(tput setaf 190)
+POWDER_BLUE=$(tput setaf 153)
+BLUE=$(tput setaf 4)
+MAGENTA=$(tput setaf 5)
+CYAN=$(tput setaf 6)
+WHITE=$(tput setaf 7)
+BRIGHT=$(tput bold)
+NORMAL=$(tput sgr0)
+BLINK=$(tput blink)
+REVERSE=$(tput smso)
+UNDERLINE=$(tput smul)
+
 ## Only choose one:
 ##    Set local router gateway address,
 ##    or have it set automagically via /sbin/ip route.
@@ -37,26 +54,10 @@ hosts_name=(
   "name3"
 )
 
-## possible text colours
-BLACK=$(tput setaf 0)
-RED=$(tput setaf 1)
-GREEN=$(tput setaf 2)
-YELLOW=$(tput setaf 3)
-LIME_YELLOW=$(tput setaf 190)
-POWDER_BLUE=$(tput setaf 153)
-BLUE=$(tput setaf 4)
-MAGENTA=$(tput setaf 5)
-CYAN=$(tput setaf 6)
-WHITE=$(tput setaf 7)
-BRIGHT=$(tput bold)
-NORMAL=$(tput sgr0)
-BLINK=$(tput blink)
-REVERSE=$(tput smso)
-UNDERLINE=$(tput smul)
-
+## Test functions
 pinghost() {
   ## Test connection to internet
-  ping $2 -c 3 > /dev/null 2>&1
+  ping $2 -c 4 > /dev/null 2>&1
 
   if [ $? -eq 0 ]
     then
@@ -66,7 +67,6 @@ pinghost() {
   fi
 }
 
-## Test functions
 interfacestate()
 {
   ## Check wlan0 interface state
