dde868b86b8c22b2c90340a31c0a6e7cb212d189
famesjranko <andrewmcdonald42@gmail.com>
2019-11-05 04:16:50 +1100

    Update pihole_tail.sh

--- pihole_tail.sh
+++ pihole_tail.sh
@@ -12,25 +12,71 @@ CYAN=$(tput setaf 6))
 BRIGHT=$(tput bold)
 NORMAL=$(tput sgr0)
 
-## print colour key
+## print header and colour key
 echo
-printf "%-16s%10s%b\n" "colour key:" ""
-printf "%-16s%10s%b\n" "    reply:" "${GREEN}${BRIGHT}GREEN${NORMAL}"
-printf "%-16s%10s%b\n" "    forward:" "${CYAN}${BRIGHT}CYAN${NORMAL}"
-printf "%-16s%10s%b\n" "    cached:" "${YELLOW}${BRIGHT}YELLOW${NORMAL}"
-printf "%-16s%10s%b\n" "    queries:" "${MAGENTA}${BRIGHT}MAGENTA${NORMAL}"
-printf "%-16s%10s%b\n" "    block list:" "${RED}${BRIGHT}RED${NORMAL}"
+echo "executing pihole.log tail . . ."
+sleep 1
+echo
+printf "%-15s%10s%b\n" "colour key:" ""
+sleep .4
+printf "%-16s%10s%b\n" "  replies:" "${GREEN}${BRIGHT}GREEN${NORMAL}"
+sleep .4
+printf "%-16s%10s%b\n" "  forwards:" "${CYAN}${BRIGHT}CYAN${NORMAL}"
+sleep .4
+printf "%-16s%10s%b\n" "  cached:" "${YELLOW}${BRIGHT}YELLOW${NORMAL}"
+sleep .4
+printf "%-16s%10s%b\n" "  queries:" "${MAGENTA}${BRIGHT}MAGENTA${NORMAL}"
+sleep .4
+printf "%-16s%10s%b\n" "  blocked:" "${RED}${BRIGHT}RED${NORMAL}"
+sleep 2
 echo
 
-## set initial line
+## set initial lines to emtpy
 line1="empty"
+line_query1="empty"
 
 while [ true ]
 do
-  ## grab wanted fields last line from log
-  line2=$(tail -n 1 /var/log/pihole.log | awk '{print $5 , $6 , $8}')
+  ## grab last line of log
+  line2=$(tail -n 1 /var/log/pihole.log)
+
+  ## grab last line of query only log
+  line_query2=$(cat /var/log/pihole.log | grep "query" | tail -n 1)
+
+  ## grab relevant fields from lines
+  line3=$(echo $line2 | awk '{print $5 , $6 , $8}')
+  line_query3=$(echo $line_query2 | awk '{print $5 , $6 , $8}')
+
+  ## compare query line latest with previous
+  ## - specifically needed as queries appear too quickly in log
+  if [ "$line_query2" !=  "$line_query1" ]
+    then
+      ## break line up into seperate fields
+      ## field 1: type
+      ## field 2: human readable address
+      ## field 3: ip address
+      q_first=$(echo $line_query3 | awk '{print $1}')
+      q_second=$(echo $line_query3 | awk '{print $2}')
+      q_third=$(echo $line_query3 | awk '{print $3}')
+      
+      ## find length of field 2
+      q_second_len=${#q_second}
+
+      ## test field 2 length and print
+      if [ $(echo "$q_second_len >= 19" | bc) -eq 1 ]
+        then
+          q_second=$(echo $q_second | cut -c -16)
+          printf "%s%s%b\n" "${MAGENTA}${BRIGHT}query${NORMAL} $q_second..." " $q_third"
+        else
+          printf "%s%s%b\n" "${MAGENTA}${BRIGHT}query${NORMAL} $q_second" " $q_third"
+      fi
+
+      ## update last line
+      line_query1=$line_query2
+  fi
   
   ## compare latest line with previous
+  ## - has queries incl. but mostly misses them - see above
   if [ "$line2" !=  "$line1" ]
     then
       ## break line up into seperate fields
@@ -40,29 +86,83 @@ do
       first=$(echo $line2 | awk '{print $1}')
       second=$(echo $line2 | awk '{print $2}')
       third=$(echo $line2 | awk '{print $3}')
+      
+      ## find length of field 2
+      second_len=${#second}
 
-      ## test for field 1 'type'
+      ## test field 1 type, then field 2 length and print
       if [ "$first" == "reply" ]
         then
-          printf "%b\n%s" "${GREEN}${BRIGHT}$second${NORMAL}"
+          if [ $(echo "$second_len >= 34" | bc) -eq 1 ]
+            then
+              second=$(echo $second | cut -c -31)
+              printf "%b\n%s" "${GREEN}${BRIGHT}$first${NORMAL} $second..."
+            else
+              printf "%b\n%s" "${GREEN}${BRIGHT}$first${NORMAL} $second"
+          fi
       elif [ "$first" == "forwarded" ]
         then
-          printf "%b\n%s" "${CYAN}${BRIGHT}$second${NORMAL}"
+          if [ $(echo "$second_len >= 30" | bc) -eq 1 ]
+            then
+              second=$(echo $second | cut -c -27)
+              printf "%b\n%s" "${CYAN}${BRIGHT}$first${NORMAL} $second..."
+            else
+              printf "%b\n%s" "${CYAN}${BRIGHT}$first${NORMAL} $second"
+          fi
       elif [ "$first" == "cached" ]
         then
-          printf "%b\n%s" "${YELLOW}${BRIGHT}$second${NORMAL}"
+          if [ $(echo "$second_len >= 33" | bc) -eq 1 ]
+            then
+              second=$(echo $second | cut -c -30)
+              printf "%b\n%s" "${YELLOW}${BRIGHT}$first${NORMAL} $second..."
+            else
+              printf "%b\n%s" "${YELLOW}${BRIGHT}$first${NORMAL} $second"
+          fi
       elif [ "$first" == "query[A]" ]
         then
-          printf "%b\n%s" "${MAGENTA}${BRIGHT}$second${NORMAL} $third"
+          if [ $(echo "$second_len >= 19" | bc) -eq 1 ]
+            then
+              second=$(echo $second | cut -c -16)
+              printf "%s%s%b\n" "${MAGENTA}${BRIGHT}query${NORMAL} $second..." " $third"
+            else
+              printf "%s%b\n" "${MAGENTA}${BRIGHT}query${NORMAL} $second $third"
+          fi
       elif [ "$first" == "query[PTR]" ]
         then
-          printf "%b\n%s" "${MAGENTA}${BRIGHT}$second${NORMAL} $third"
+          if [ $(echo "$second_len >= 19" | bc) -eq 1 ]
+            then
+              second=$(echo $second | cut -c -16)
+              printf "%s%s%b\n" "${MAGENTA}${BRIGHT}query${NORMAL} $second..." " $third"
+            else
+              printf "%b\n%s" "${MAGENTA}${BRIGHT}query${NORMAL} $second $third"
+          fi
+      elif [ "$first" == "query[AAAA]" ]
+        then
+          if [ $(echo "$second_len >= 19" | bc) -eq 1 ]
+            then
+              second=$(echo $second | cut -c -16)
+              printf "%s%s%b\n" "${MAGENTA}${BRIGHT}query${NORMAL} $second..." " $third"
+            else
+              printf "%b\n%s" "${MAGENTA}${BRIGHT}query${NORMAL} $second $third"
+          fi
       elif [ "$first" == "/etc/pihole/gravity.list" ]
         then
-          printf "%b\n%s" "${RED}${BRIGHT}$second${NORMAL}"
+          if [ $(echo "$second_len >= 32" | bc) -eq 1 ]
+            then
+              second=$(echo $second | cut -c -29)
+              printf "%b\n%s" "${RED}${BRIGHT}blocked${NORMAL} $second..."
+            else
+              printf "%b\n%s" "${RED}${BRIGHT}blocked${NORMAL} $second"
+          fi
       elif [ "$first" ==  "/etc/pihole/black.list" ]
         then
-          printf "%b\n%s" "${RED}${BRIGHT}$second${NORMAL}"
+          if [ $(echo "$second_len >= 32" | bc) -eq 1 ]
+            then
+              second=$(echo $second | cut -c -29)
+              printf "%b\n%s" "${RED}${BRIGHT}blocked${NORMAL} $second..."
+            else
+              printf "%b\n%s" "${RED}${BRIGHT}blocked${NORMAL} $second"
+          fi
       else
           printf "%b\n%s" "$first $third"
       fi
