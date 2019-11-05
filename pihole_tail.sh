3bc5b6c42013e1d476de86b6e5f808aa0cdc7dde
famesjranko <andrewmcdonald42@gmail.com>
2019-11-05 20:51:53 +1100

    Update pihole_tail.sh

--- pihole_tail.sh
+++ pihole_tail.sh
@@ -1,6 +1,6 @@
 ##   Output structured for 2.2" pi-tft display hat.
 ##
-##   A simple looping script that mimics and colourises 
+##   A simple looping script that mimics and colourises
 ##   the output of 'tail -f /var/log/pihole.log' for the
 ##   raspberry pi 2.2" pi-tft display hat.
 ##
@@ -20,7 +20,7 @@ RED=$(tput setaf 1)
 GREEN=$(tput setaf 2)
 YELLOW=$(tput setaf 3)
 MAGENTA=$(tput setaf 5)
-CYAN=$(tput setaf 6))
+CYAN=$(tput setaf 6)
 BRIGHT=$(tput bold)
 NORMAL=$(tput sgr0)
 
@@ -43,34 +43,35 @@ printf "%-16s%10s%b\n" "  blocked:" "${RED}${BRIGHT}RED${NORMAL}"
 sleep 2
 echo
 
-## set initial lines to emtpy
-line_query1="empty"
+## set initial lines to anything
 line1="empty"
+line_query1="empty"
 
+## loop forever
 while [ true ]
 do
   ## grab last line of query only log
   line_query2=$(cat /var/log/pihole.log | grep "query" | tail -n 1)
 
-  ## grab relevant fields from line
+  ## grab relevant line fields
   line_query3=$(echo $line_query2 | awk '{print $5 , $6 , $8}')
 
   ## compare query line latest with previous
   ## - specifically needed as queries appear too quickly in log
   if [ "$line_query3" !=  "$line_query1" ]
     then
-      ## break line up into seperate fields
+      ## atomise line fields
       ## field 1: type
       ## field 2: human readable address
       ## field 3: ip address
       q_first=$(echo $line_query3 | awk '{print $1}')
       q_second=$(echo $line_query3 | awk '{print $2}')
       q_third=$(echo $line_query3 | awk '{print $3}')
-      
-      ## find length of field 2
+
+      ## determine field 2 length
       q_second_len=${#q_second}
 
-      ## test field 2 length and print
+      ## colourised print
       if [ $(echo "$q_second_len >= 19" | bc) -eq 1 ]
         then
           q_second=$(echo $q_second | cut -c -16)
@@ -82,29 +83,29 @@ do
       ## update last line
       line_query1=$line_query3
   fi
-  
-  ## grab last line of log
+
+  ## grab last line of log proper
   line2=$(tail -n 1 /var/log/pihole.log)
-  
-  ## grab relevant fields from line
+
+  ## grab relevant line fields
   line3=$(echo $line2 | awk '{print $5 , $6 , $8}')
-  
+
   ## compare latest line with previous
   ## - has queries incl. but mostly misses them - see above
   if [ "$line3" !=  "$line1" ] && [ "$line3" != "$line_query1" ]
     then
-      ## break line up into seperate fields
+      ## atomise line fields
       ## field 1: type
       ## field 2: human readable address
       ## field 3: ip address
-      first=$(echo $line2 | awk '{print $1}')
-      second=$(echo $line2 | awk '{print $2}')
-      third=$(echo $line2 | awk '{print $3}')
-      
-      ## find length of field 2
+      first=$(echo $line3 | awk '{print $1}')
+      second=$(echo $line3 | awk '{print $2}')
+      third=$(echo $line3 | awk '{print $3}')
+
+      ## determine field 2 length
       second_len=${#second}
 
-      ## test field 1 type, then field 2 length and print
+      ## colourised print
       if [ "$first" == "reply" ]
         then
           if [ $(echo "$second_len >= 34" | bc) -eq 1 ]
@@ -151,13 +152,13 @@ do
               printf "%b\n%s" "${RED}${BRIGHT}blocked${NORMAL} $second"
           fi
       else
-          printf "%b\n%s" "$first $third"
+          printf "%b\n%s" "$first $second"
       fi
-      
-      ## update previous line
+
+      ## set most recent line to last
       line1=$line3
   fi
-  
-  ## wait to loop again
+
+  ## wait before looping
   sleep .5
 done
