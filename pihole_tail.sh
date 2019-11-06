bf6ad75959a1de9e5d988c19e9be42119b3248f9
famesjranko <andrewmcdonald42@gmail.com>
2019-11-06 14:55:17 +1100

    Update pihole_tail.sh

--- pihole_tail.sh
+++ pihole_tail.sh
@@ -31,9 +31,9 @@ sleep 1
 echo
 printf "%-15s%10s%b\n" "colour key:" ""
 sleep .4
-printf "%-16s%10s%b\n" "  replies:" "${CYAN}${BRIGHT}GREEN${NORMAL}"
+printf "%-16s%10s%b\n" "  replies:" "${GREEN}${BRIGHT}GREEN${NORMAL}"
 sleep .4
-printf "%-16s%10s%b\n" "  forwards:" "${GREEN}${BRIGHT}CYAN${NORMAL}"
+printf "%-16s%10s%b\n" "  forwards:" "${CYAN}${BRIGHT}CYAN${NORMAL}"
 sleep .4
 printf "%-16s%10s%b\n" "  cached:" "${YELLOW}${BRIGHT}YELLOW${NORMAL}"
 sleep .4
@@ -46,6 +46,7 @@ echo
 ## set initial lines to anything
 line1="empty"
 line_query1="empty"
+line_forward1="empty"
 
 ## loop forever
 while [ true ]
@@ -58,6 +59,10 @@ do
   ## grab last line of query only log, then grab relevant line fields
   line_query2=$(cat /var/log/pihole.log | grep "query" | tail -n 1)
   line_query3=$(echo $line_query2 | awk '{print $5 , $6 , $8}')
+  
+  ## grab last line of forwarded only log, then grab relevant fields
+  line_forward2=$(cat /var/log/pihole.log | grep "forwarded" | tail -n 1)
+  line_forward3=$(echo $line_query2 | awk '{print $5 , $6 , $8}')
 
   ## compare query line latest with previous
   ## - specifically needed as queries appear too quickly in log
@@ -68,7 +73,7 @@ do
       ## field 1: type
       ## field 2: human readable address
       ## field 3: ip address
-      q_first=$(echo $line_query3 | awk '{print $1}')
+      #q_first=$(echo $line_query3 | awk '{print $1}')
       q_second=$(echo $line_query3 | awk '{print $2}')
       q_third=$(echo $line_query3 | awk '{print $3}')
 
@@ -91,12 +96,39 @@ do
       line_query1=$line_query3
   fi
 
+  ## compare query line latest with previous
+  ## - specifically needed as queries appear too quickly in log
+  ## - test for 'wpad.LandOfOz' is specific to my network; can remove
+  if [ "$line_forward3" !=  "$line_forward1" -a "$(echo $line_forward3 | awk '{print $2}')" != "wpad.LandOfOz" ]
+    then
+      ## atomise line fields
+      ## field 1: type
+      ## field 2: human readable address
+      ## field 3: ip address
+      #f_first=$(echo $line_forward3 | awk '{print $1}')
+      f_second=$(echo $line_forward3 | awk '{print $2}')
 
+      ## determine field 2 length
+      f_second_len=${#f_second}
+
+      ## start new line
+      printf "%b\n"
+
+      ## colourised print
+      if [ $(echo "$f_second_len >= 34" | bc) -eq 1 ]
+        then
+          f_second=$(echo $f_second | cut -c -31)
+          printf "%6s" "${CYAN}${BRIGHT}forwd${NORMAL} " "$f_second..."
+        else
+          printf "%6s" "${CYAN}${BRIGHT}forwd${NORMAL} " "$f_second"
+      fi
+      ## update last line
+      line_forward1=$line_forward3
+  fi
 
   ## compare latest line proper with previous
-  ## - has queries incl. but mostly misses them - see above
   ## - test for 'wpad.LandOfOz' is specific to my network; can remove
-  if [ "$line3" !=  "$line1" -a "$line3" != "$line_query1" -a "$(echo $line3 | awk '{print $2}')" != "wpad.LandOfOz" ]
+  if [ "$line3" !=  "$line1" -a "$(echo $line3 | awk '{print $2}')" != "wpad.LandOfOz" ]
     then
       ## atomise line fields
       ## field 1: type
@@ -104,7 +136,7 @@ do
       ## field 3: ip address
       first=$(echo $line3 | awk '{print $1}')
       second=$(echo $line3 | awk '{print $2}')
-      third=$(echo $line3 | awk '{print $3}')
+      #third=$(echo $line3 | awk '{print $3}')
 
       ## determine field 2 length
       second_len=${#second}
@@ -118,45 +150,27 @@ do
           if [ $(echo "$second_len >= 34" | bc) -eq 1 ]
             then
               second=$(echo $second | cut -c -31)
-              printf "%s" "${CYAN}${BRIGHT}$first${NORMAL} $second..."
+              printf "%s" "${GREEN}${BRIGHT}reply{NORMAL} $second..."
             else
-              printf "%s" "${CYAN}${BRIGHT}$first${NORMAL} $second"
-          fi
-      elif [ "$first" = "forwarded" ]
-        then
-          if [ $(echo "$second_len >= 30" | bc) -eq 1 ]
-            then
-              second=$(echo $second | cut -c -27)
-              printf "%s" "${GREEN}${BRIGHT}$first${NORMAL} $second..."
-            else
-              printf "%s" "${GREEN}${BRIGHT}$first${NORMAL} $second"
+              printf "%s" "${GREEN}${BRIGHT}reply${NORMAL} $second"
           fi
       elif [ "$first" = "cached" ]
         then
           if [ $(echo "$second_len >= 33" | bc) -eq 1 ]
             then
               second=$(echo $second | cut -c -30)
-              printf "%s" "${YELLOW}${BRIGHT}$first${NORMAL} $second..."
+              printf "%s" "${YELLOW}${BRIGHT}cache${NORMAL} $second..."
             else
-              printf "%s" "${YELLOW}${BRIGHT}$first${NORMAL} $second"
-          fi
-      elif [ "$first" = "query[A]" -o "$first" = "query[PTR]" -o "$first" = "query[AAAA]" ]
-        then
-          if [ $(echo "$second_len >= 20" | bc) -eq 1 ]
-            then
-              second=$(echo $second | cut -c -17)
-              printf "%-6s%-20s%14s" "${MAGENTA}${BRIGHT}query ${NORMAL}" "$second..." "$third"
-            else
-              printf "%-6s%-20s%14s" "${MAGENTA}${BRIGHT}query ${NORMAL}" "$second" "$third"
+              printf "%s" "${YELLOW}${BRIGHT}cache${NORMAL} $second"
           fi
       elif [ "$first" = "/etc/pihole/gravity.list" -o "$first" = "/etc/pihole/black.list" -o "$first" = "/etc/pihole/regex.list" ]
         then
           if [ $(echo "$second_len >= 32" | bc) -eq 1 ]
             then
               second=$(echo $second | cut -c -29)
-              printf "%s" "${RED}${BRIGHT}blocked${NORMAL} $second..."
+              printf "%s" "${RED}${BRIGHT}block${NORMAL} $second..."
             else
-              printf "%s" "${RED}${BRIGHT}blocked${NORMAL} $second"
+              printf "%s" "${RED}${BRIGHT}block${NORMAL} $second"
           fi
       else
           printf "%s" "$first $second"
